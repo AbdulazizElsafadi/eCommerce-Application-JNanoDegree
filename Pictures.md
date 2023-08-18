49a279fad27a33c58d421c029f89fa8658896883
Abdulaziz Alsafadi <azeez.h.749@gmail.com>
2023-08-18 16:48:42 +0300

    adding required pictures

---
+++ Pictures.md
@@ -0,0 +1,45 @@
+# Required Pictures
+
+## Splunk
+
+### 1- Splunk Global Configuration 
+![](../../Screenshot 1445-01-26 at 4.44.44 PM.png)
+
+### 2- Creating HTTP Event Collector to index all *log statements* in the application to Spunk
+![](../../Screenshot 1445-01-26 at 4.44.37 PM.png)
+
+### 3- HTTP Event Collector is created successfully
+![](../../Screenshot 1445-01-26 at 4.44.31 PM.png)
+
+### 4- Splunk Configuration in the Code *log4j2-spring.xml*
+![](../../new1.png)
+
+### 5- making the search with the created index *spring_dev*
+![](../../Screenshot 1445-01-26 at 4.44.12 PM.png)
+
+### 6- Creating an alert of *successful creation of users*
+![](../../Screenshot 1445-01-26 at 4.30.49 PM.png)
+
+### 7- Alert is created successfully
+![](../../Screenshot 1445-01-26 at 4.31.03 PM.png)
+![](../../Screenshot 1445-01-26 at 4.41.10 PM.png)
+
+### 8- Searching with the alert
+![](../../Screenshot 1445-01-26 at 4.33.45 PM.png)
+
+## Jenkins
+
+### 1- Jenkins maven Job
+![](../../../../../var/folders/_m/rk2x1rz57b5cc_9qw5m_w6xc0000gn/T/TemporaryItems/NSIRD_screencaptureui_cShyG2/Screenshot 1445-02-02 at 4.35.43 PM.png)
+
+### 2- Connecting the Job with GitHub
+![](../../Screenshot 1445-02-02 at 4.16.54 PM.png)
+
+### 3- Locating pom.xml file of the Project to Jenkins
+![](../../Screenshot 1445-02-02 at 4.17.06 PM.png)
+
+### 4- Executing the Job
+![](../../Screenshot 1445-02-02 at 4.17.40 PM.png)
+![](../../Screenshot 1445-02-02 at 4.17.45 PM.png)
+![](../../Screenshot 1445-02-02 at 4.17.59 PM.png)
+![](../../Screenshot 1445-02-02 at 4.18.16 PM.png)
--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -16,55 +16,60 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user")
 public class UserController {
 
-	Logger logger = LogManager.getLogger(UserController.class);
+    Logger logger = LogManager.getLogger(UserController.class);
 
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private CartRepository cartRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private CartRepository cartRepository;
 
-	@GetMapping("/id/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		logger.info("UserController: findById execution started...");
-		return ResponseEntity.of(userRepository.findById(id));
-	}
-	
-	@GetMapping("/{username}")
-	public ResponseEntity<User> findByUserName(@PathVariable String username) {
-		logger.info("UserController: findByUserName execution started...");
-		User user = userRepository.findByUsername(username);
-		logger.info("UserController: user is {}", user);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
-	}
-	
-	@PostMapping("/create")
-	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
-		logger.info("UserController: createUser execution started...");
-		if(createUserRequest.getUsername() == null || createUserRequest.getPassword() == null || createUserRequest.getConfirmPassword() == null) {
-			logger.error("you didn't provider username or password or confirmPassword!");
-			return ResponseEntity.badRequest().build();
-		}
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-		if(createUserRequest.getPassword().length() < 7 || !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
-			logger.error("password is either smaller than 7 digits or confirm password is not the same as the password!");
-			return ResponseEntity.badRequest().build();
-		}
+    @GetMapping("/id/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        logger.info("UserController: findById execution started...");
+        return ResponseEntity.of(userRepository.findById(id));
+    }
 
-		User user = new User();
-		user.setUsername(createUserRequest.getUsername());
-		Cart cart = new Cart();
-		cartRepository.save(cart);
-		user.setCart(cart);
-		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
-		userRepository.save(user);
-		logger.info("User created successfully with username: {}", user.getUsername());
-		logger.info("UserController: createUser execution ended...");
+    @GetMapping("/{username}")
+    public ResponseEntity<User> findByUserName(@PathVariable String username) {
+        logger.info("UserController: findByUserName execution started...");
+        User user = userRepository.findByUsername(username);
+        logger.info("UserController: user is {}", user);
+        return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+    }
 
-		return ResponseEntity.ok(user);
-	}
-	
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        logger.info("UserController: createUser execution started...");
+        if (createUserRequest.getUsername() == null || createUserRequest.getPassword() == null || createUserRequest.getConfirmPassword() == null) {
+            logger.error("you didn't provider username or password or confirmPassword!");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (createUserRequest.getPassword().length() < 7 || !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+            logger.error("password is either smaller than 7 digits or confirm password is not the same as the password!");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (userRepository.findByUsername(createUserRequest.getUsername()) != null) {
+            logger.error("username already exists {}", createUserRequest.getUsername());
+            return ResponseEntity.badRequest().build();
+        }
+
+        User user = new User();
+        user.setUsername(createUserRequest.getUsername());
+        Cart cart = new Cart();
+        cartRepository.save(cart);
+        user.setCart(cart);
+        user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+        userRepository.save(user);
+        logger.info("User created successfully with username: {}", user.getUsername());
+        logger.info("UserController: createUser execution ended...");
+
+        return ResponseEntity.ok(user);
+    }
+
 }
